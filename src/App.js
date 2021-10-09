0936206c886113bcb067e5a896f83f012fc125c0
avelar96 <88787621+avelar96@users.noreply.github.com>
2021-10-09 10:26:21 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/footer';
+import NavBar from "./components/navBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      
+      <h1>Welcome to my awesome store!</h1>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
